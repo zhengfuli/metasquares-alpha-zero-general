b280fa9b7b226163d78347fa82eaecfe234be513
zhengfuli <zhengfuli@bupt.edu.cn>
2018-04-18 00:07:37 +0800

    added NN files

--- metasquares/pytorch/NNet.py
+++ metasquares/pytorch/NNet.py
@@ -16,7 +16,7 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 
-from .OthelloNNet import OthelloNNet as onnet
+from .MetaSquaresNNet import MetaSquaresNNet as onnet
 
 args = dotdict({
     'lr': 0.001,
